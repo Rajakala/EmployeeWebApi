76f55ce2279eed2b9d26823b68448366e53aa634
Rajakala <rajakalareddy7@gmail.com>
2021-08-12 13:17:19 -0700

    Initial commit

---
+++ EmployeeMVCApp/Controllers/EmployeeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using EmployeeWebApi.Models;
+
+namespace EmployeeMVCApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public async Task< ActionResult> Index()
+        {
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            employees = await List();
+            return View(employees);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            var employees = await List();
+            var editableEmployee = employees.FirstOrDefault(e => e.ID == id);
+            return View(editableEmployee);
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            var employees = await List();
+            var deletableEmployee = employees.FirstOrDefault(e => e.ID == id);
+            return View(deletableEmployee);
+        }
+
+        public async Task<ActionResult> DeleteEmployee(int id)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44301/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await client.DeleteAsync("api/Employee/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return null;
+        }
+
+        public async Task<ActionResult> UpdateEmployee(EmployeeModel employee)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44301/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await client.PutAsJsonAsync("api/Employee", employee);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return null;
+        }
+
+        public async Task<ActionResult> AddEmployee(EmployeeModel employee)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44301/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await client.PostAsJsonAsync("api/Employee",employee);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return null;
+        }
+
+        public async Task<List<EmployeeModel>> List()
+        {
+            
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44301/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await client.GetAsync("api/Employee");
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsAsync<List<EmployeeModel>>();
+            }
+            return null;
+        }
+    }
+}
---
+++ EmployeeWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using EmployeeWebApi.Models;
+
+namespace EmployeeWebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        // GET api/<controller>
+        public async Task<IEnumerable<EmployeeModel>> Get()
+        {
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            var sqlConnectionString = ConfigurationManager.ConnectionStrings["EmployeeDB"].ToString();
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionString);
+            SqlCommand sqlCommand = new SqlCommand("GetEmployees", sqlConnection);
+            sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            sqlConnection.Open();
+            using(var sqlDataReader = await sqlCommand.ExecuteReaderAsync())
+            {
+                var OrdinalID = sqlDataReader.GetOrdinal("ID");
+                var OrdinalFirstName = sqlDataReader.GetOrdinal("FirstName");
+                var OrdinalLastName = sqlDataReader.GetOrdinal("LastName");
+                var OrdinalGender = sqlDataReader.GetOrdinal("Gender");
+                var OrdinalSalary = sqlDataReader.GetOrdinal("Salary");
+                while (sqlDataReader.Read())
+                {
+                    var employee = new EmployeeModel();
+                    employee.ID = Convert.ToInt32(sqlDataReader.GetValue(OrdinalID));
+                    employee.FirstName = Convert.ToString(sqlDataReader.GetValue(OrdinalFirstName));
+                    employee.LastName = Convert.ToString(sqlDataReader.GetValue(OrdinalLastName));
+                    employee.Gender = Convert.ToString(sqlDataReader.GetValue(OrdinalGender));
+                    employee.Salary = Convert.ToInt32(sqlDataReader.GetValue(OrdinalSalary));
+                    employees.Add(employee);
+                }
+            }
+
+            return employees;
+        }
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> Create(EmployeeModel employee)
+        {
+            var sqlConnectionString = ConfigurationManager.ConnectionStrings["EmployeeDB"].ToString();
+            var sqlConnection = new SqlConnection(sqlConnectionString);
+            SqlCommand sqlCommand = new SqlCommand("InsertEmployee", sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@FirstName", employee.FirstName);
+            sqlCommand.Parameters.AddWithValue("@LastName", employee.LastName);
+            sqlCommand.Parameters.AddWithValue("@Gender", employee.Gender);
+            sqlCommand.Parameters.AddWithValue("@Salary", employee.Salary);
+            sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            sqlConnection.Open();
+            var result = await sqlCommand.ExecuteNonQueryAsync();
+            if(sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+            if (result > 0)
+            {
+                return Request.CreateResponse(HttpStatusCode.Created);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        [HttpPut]
+        public async Task<HttpResponseMessage> UpdateEmployee(EmployeeModel employee)
+        {
+            var sqlConnectionString = ConfigurationManager.ConnectionStrings["EmployeeDB"].ToString();
+            var sqlConnection = new SqlConnection(sqlConnectionString);
+            SqlCommand sqlCommand = new SqlCommand("UpdateEmployee", sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@ID", employee.ID);
+            sqlCommand.Parameters.AddWithValue("@FirstName", employee.FirstName);
+            sqlCommand.Parameters.AddWithValue("@LastName", employee.LastName);
+            sqlCommand.Parameters.AddWithValue("@Gender", employee.Gender);
+            sqlCommand.Parameters.AddWithValue("@Salary", employee.Salary);
+            sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            sqlConnection.Open();
+            var result = await sqlCommand.ExecuteNonQueryAsync();
+            if(sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+            if (result > 0)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<HttpResponseMessage> DeleteEmployee(int id)
+        {
+            var sqlConnectionString = ConfigurationManager.ConnectionStrings["EmployeeDB"].ToString();
+            var sqlConnection = new SqlConnection(sqlConnectionString);
+            SqlCommand sqlCommand = new SqlCommand("DeleteEmployee", sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@ID", id);
+            sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            sqlConnection.Open();
+            var result = await sqlCommand.ExecuteNonQueryAsync();
+            if(sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+            if (result > 0)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+       
+
+       
+    }
+}
